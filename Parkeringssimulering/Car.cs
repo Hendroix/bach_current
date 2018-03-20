dddf0cdedffe5ae1d188172f4f72508eccaac78c
Aga95 <aga-11@hotmail.com>
2018-03-20 11:42:33 +0100

    simulering v1
    Starten av simuleringen

---
+++ Parkeringssimulering/Car.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Simulering
+{
+	class Car
+	{
+		
+	}
+}
---
+++ Parkeringssimulering/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Simulering
+{
+	class Main
+	{
+		public static void Main(string[]args)
+		{
+			
+		}
+	}
+}
